b4f1c26df555d7876b4f8635fe59e1641e1c0c2c
TechKind <ken_hayakawa@tech-kind.biz>
2022-03-04 23:20:10 +0900

    PathManagerのBaseを作成

---
+++ PathManager/PathManagerModule.cs
@@ -0,0 +1,29 @@
+﻿using MessagePipe;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Prism.Ioc;
+using Prism.Modularity;
+
+namespace PathManager
+{
+    public class PathManagerModule : IModule
+    {
+        public async void OnInitialized(IContainerProvider containerProvider)
+        {
+            IHost host = Host.CreateDefaultBuilder()
+            .ConfigureServices(services =>
+            {
+                services.AddMessagePipe();
+                services.AddHostedService<PathManager>();
+            })
+            .Build();
+
+            await host.RunAsync();
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
